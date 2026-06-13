1f1897aff6b321673a619be75ce9afd5fa5fa05d
asgirind-dev <asgiriinduwara@gmail.com>
2026-06-14 01:15:58 +0530

    refactor: Doing changes in backend folder

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 
-// අපේ අලුත් Auth Routes ෆයිල් එක මෙතනට ලෝඩ් කරනවා
+
 const authRoutes = require('./routes/authRoutes');
 
 const app = express();
@@ -13,10 +13,9 @@ app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Routing Middleware 
-// (Frontend එකෙන් '/api/auth' වලට එන හැම request එකක්ම authRoutes එකට පාස් කරනවා)
 app.use('/api/auth', authRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running cleanly on port ${PORT}`);
+  console.log(`Server running cleanly on port ${PORT}`);
 });
