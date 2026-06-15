da1e0695a11ecbe297320eceaa149eb4408cebd9
batheesha <batheeshahasaranga123@gmail.com>
2026-06-15 22:51:36 +0530

    feat: delete dublicate page Examtakepage in routes.jsx

--- frontend/src/router/routes.jsx
+++ frontend/src/router/routes.jsx
@@ -47,7 +47,6 @@ import ExamQualityAuditsPage from "../pages/validator/ExamQualityAuditsPage";
 // Student Pages
 import StudentDashboard from '../pages/student/StudentDashboard';
 import MyExamsPage from '../pages/student/MyExamsPage';
-import ExamTakePage from '../pages/student/ExamTakePage';
 import MarketplacePage from '../pages/student/MarketplacePage';
 import SubscriptionPage from '../pages/student/SubscriptionPage';
 import ExamResultsPage from '../pages/student/ExamResultsPage';
